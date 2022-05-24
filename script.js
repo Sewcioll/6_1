445bf434434fcddf8daea7c06659acf307ff82fc
Seweryn Gajewski <100691797+Sewcioll@users.noreply.github.com>
2022-05-24 11:43:41 +0000

    6_2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (function () {
   const ex1 = document.getElementById('ex1_button')
   const ex1c = document.getElementById('ex1_content')
+  const ex2 = document.getElementById('ex2_button')
+  const ex2c = document.getElementById('ex2_content')
 
   ex1.addEventListener("click",function(){
     var data = "";
@@ -12,5 +14,35 @@
       }
     ex1c.innerHTML=data;
   })
+ 
+    var nr = document.getElementById("ex2_text");
+    const log = document.getElementById("ex2_content");
+    const log1 = document.getElementById("ex3_content");
+    const log2 = document.getElementById("ex4_content");
+    const log3 = document.getElementById("ex2_content");
+    nr.addEventListener("input",function(){
+      if(nr.value.match(/^[0-9]+$/)&&nr.value.length==9)
+        log.textContent="Numer telefonu jest poprawny";
+      if(nr.value.match(/[\w]/))
+        log1.textContent="Numer nie może zawierać znaków specjalnych";
+      if(nr.value.length!=9)
+        log2.textContent="Dlugość numeru musi być równa 9";
+      else
+        log.textContent=" ";
+      if(nr.value.match(/a-zA-Z/))
+        log3.textContent="Numer nie moze zawierac liter";
+      else
+        log.textContent=" ";
+    
+      
+      
+    
+  })
+
+   
+      
   
+  
+  
+    
 })();
